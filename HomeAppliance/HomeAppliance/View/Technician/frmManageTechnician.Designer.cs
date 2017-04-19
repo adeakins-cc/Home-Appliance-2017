2f8d8faf0855bfca94958d41d39fe59317ad09f8
Ken Thai <kenzknight01@gmail.com>
2017-04-19 19:42:41 -0400

    Tech done

--- HomeAppliance/HomeAppliance/View/Technician/frmManageTechnician.Designer.cs
+++ HomeAppliance/HomeAppliance/View/Technician/frmManageTechnician.Designer.cs
@@ -35,17 +35,18 @@
             this.technicianTableAdapter = new HomeAppliance.HomeAppDBDataSetTableAdapters.TechnicianTableAdapter();
             this.fillByTechnicianToolStrip = new System.Windows.Forms.ToolStrip();
             this.fillByTechnicianToolStripButton = new System.Windows.Forms.ToolStripButton();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTech = new System.Windows.Forms.Button();
+            this.txtTechName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnEditTech = new System.Windows.Forms.Button();
+            this.btnDeleteTech = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.lstTechList = new System.Windows.Forms.ListBox();
+            this.dgvTechList = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.technicianBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.homeAppDBDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.homeAppDBDataSet)).BeginInit();
             this.fillByTechnicianToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTechList)).BeginInit();
             this.SuspendLayout();
             // 
             // technicianBindingSource
@@ -74,7 +75,7 @@
             this.fillByTechnicianToolStripButton});
             this.fillByTechnicianToolStrip.Location = new System.Drawing.Point(0, 0);
             this.fillByTechnicianToolStrip.Name = "fillByTechnicianToolStrip";
-            this.fillByTechnicianToolStrip.Size = new System.Drawing.Size(226, 25);
+            this.fillByTechnicianToolStrip.Size = new System.Drawing.Size(316, 27);
             this.fillByTechnicianToolStrip.TabIndex = 1;
             this.fillByTechnicianToolStrip.Text = "fillByTechnicianToolStrip";
             // 
@@ -82,89 +83,95 @@
             // 
             this.fillByTechnicianToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.fillByTechnicianToolStripButton.Name = "fillByTechnicianToolStripButton";
-            this.fillByTechnicianToolStripButton.Size = new System.Drawing.Size(96, 22);
+            this.fillByTechnicianToolStripButton.Size = new System.Drawing.Size(118, 24);
             this.fillByTechnicianToolStripButton.Text = "FillByTechnician";
-            this.fillByTechnicianToolStripButton.Click += new System.EventHandler(this.fillByTechnicianToolStripButton_Click);
             // 
-            // button1
+            // btnAddTech
             // 
-            this.button1.Location = new System.Drawing.Point(12, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Add Technician";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAddTech.Location = new System.Drawing.Point(13, 254);
+            this.btnAddTech.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddTech.Name = "btnAddTech";
+            this.btnAddTech.Size = new System.Drawing.Size(135, 28);
+            this.btnAddTech.TabIndex = 2;
+            this.btnAddTech.Text = "Add Technician";
+            this.btnAddTech.UseVisualStyleBackColor = true;
+            this.btnAddTech.Click += new System.EventHandler(this.btnAddTech_Click);
             // 
-            // textBox1
+            // txtTechName
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 177);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtTechName.Location = new System.Drawing.Point(69, 218);
+            this.txtTechName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTechName.Name = "txtTechName";
+            this.txtTechName.Size = new System.Drawing.Size(235, 22);
+            this.txtTechName.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 180);
+            this.label1.Location = new System.Drawing.Point(13, 222);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(49, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Name:";
             // 
-            // button2
+            // btnEditTech
             // 
-            this.button2.Location = new System.Drawing.Point(119, 206);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Edit Technician";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnEditTech.Location = new System.Drawing.Point(168, 254);
+            this.btnEditTech.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEditTech.Name = "btnEditTech";
+            this.btnEditTech.Size = new System.Drawing.Size(135, 28);
+            this.btnEditTech.TabIndex = 5;
+            this.btnEditTech.Text = "Edit Technician";
+            this.btnEditTech.UseVisualStyleBackColor = true;
+            this.btnEditTech.Click += new System.EventHandler(this.btnEditTech_Click);
             // 
-            // button3
+            // btnDeleteTech
             // 
-            this.button3.Location = new System.Drawing.Point(11, 236);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Delete Technician";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDeleteTech.Location = new System.Drawing.Point(13, 291);
+            this.btnDeleteTech.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeleteTech.Name = "btnDeleteTech";
+            this.btnDeleteTech.Size = new System.Drawing.Size(135, 28);
+            this.btnDeleteTech.TabIndex = 6;
+            this.btnDeleteTech.Text = "Delete Technician";
+            this.btnDeleteTech.UseVisualStyleBackColor = true;
+            this.btnDeleteTech.Click += new System.EventHandler(this.btnDeleteTech_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(119, 236);
+            this.btnExit.Location = new System.Drawing.Point(168, 291);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(88, 23);
+            this.btnExit.Size = new System.Drawing.Size(135, 28);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // lstTechList
+            // dgvTechList
             // 
-            this.lstTechList.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.technicianBindingSource, "technicianId", true));
-            this.lstTechList.DataSource = this.technicianBindingSource;
-            this.lstTechList.DisplayMember = "name";
-            this.lstTechList.FormattingEnabled = true;
-            this.lstTechList.Location = new System.Drawing.Point(10, 25);
-            this.lstTechList.Name = "lstTechList";
-            this.lstTechList.Size = new System.Drawing.Size(198, 147);
-            this.lstTechList.TabIndex = 0;
-            this.lstTechList.ValueMember = "technicianId";
+            this.dgvTechList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTechList.Location = new System.Drawing.Point(13, 30);
+            this.dgvTechList.Name = "dgvTechList";
+            this.dgvTechList.RowTemplate.Height = 24;
+            this.dgvTechList.Size = new System.Drawing.Size(291, 181);
+            this.dgvTechList.TabIndex = 8;
+            this.dgvTechList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dgvTechList_MouseClick);
             // 
             // frmManageTechnician
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(226, 284);
+            this.ClientSize = new System.Drawing.Size(316, 367);
+            this.Controls.Add(this.dgvTechList);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnDeleteTech);
+            this.Controls.Add(this.btnEditTech);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTechName);
+            this.Controls.Add(this.btnAddTech);
             this.Controls.Add(this.fillByTechnicianToolStrip);
-            this.Controls.Add(this.lstTechList);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmManageTechnician";
             this.Text = "frmManageTechnician";
             this.Load += new System.EventHandler(this.frmManageTechnician_Load);
@@ -173,6 +180,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.homeAppDBDataSet)).EndInit();
             this.fillByTechnicianToolStrip.ResumeLayout(false);
             this.fillByTechnicianToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTechList)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,12 +193,12 @@
         private HomeAppDBDataSetTableAdapters.TechnicianTableAdapter technicianTableAdapter;
         private System.Windows.Forms.ToolStrip fillByTechnicianToolStrip;
         private System.Windows.Forms.ToolStripButton fillByTechnicianToolStripButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTech;
+        private System.Windows.Forms.TextBox txtTechName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnEditTech;
+        private System.Windows.Forms.Button btnDeleteTech;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.ListBox lstTechList;
+        private System.Windows.Forms.DataGridView dgvTechList;
     }
 }
--- HomeAppliance/HomeAppliance/View/Technician/frmManageTechnician.cs
+++ HomeAppliance/HomeAppliance/View/Technician/frmManageTechnician.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace HomeAppliance.View.Technician
 {
     public partial class frmManageTechnician : Form
     {
+        SqlConnection dbConn = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=HomeAppDB;Integrated Security=True");
+        SqlCommand dbCommand = new SqlCommand();
+        bool buttonClicked;
+
         public frmManageTechnician()
         {
             InitializeComponent();
@@ -21,30 +26,102 @@ namespace HomeAppliance.View.Technician
         {
             // TODO: This line of code loads data into the 'homeAppDBDataSet.Technician' table. You can move, or remove it, as needed.
             this.technicianTableAdapter.Fill(this.homeAppDBDataSet.Technician);
-
+            displayTech();
         }
 
-        private void fillByTechnicianToolStripButton_Click(object sender, EventArgs e)
+        //private void fillByTechnicianToolStripButton_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        this.technicianTableAdapter.FillByTechnician(this.homeAppDBDataSet.Technician);
+        //    }
+        //    catch (System.Exception ex)
+        //    {
+        //        System.Windows.Forms.MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        private void displayTech()
+        {
+            string getTechList = "SELECT * FROM Technician ORDER BY name ASC";
+            DataTable dt = new DataTable();
+            SqlDataAdapter techAdapter = new SqlDataAdapter(getTechList, dbConn);
+            techAdapter.Fill(dt);
+            dgvTechList.DataSource = dt;
+            dgvTechList.Columns[0].Visible = false;
+        }
+
+        private void executeQuery(String query)
         {
             try
             {
-                this.technicianTableAdapter.FillByTechnician(this.homeAppDBDataSet.Technician);
+                dbConn.Open();
+                dbCommand = new SqlCommand(query, dbConn);
+                if (dbCommand.ExecuteNonQuery() == 1)
+                {
+                    if (buttonClicked == true)
+                    {
+                        MessageBox.Show("New Technician is added");
+                    }
+                    else if (buttonClicked == false)
+                    {
+                        MessageBox.Show("Successfully delete technician");
+                    }
+                }
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
-                System.Windows.Forms.MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message);
             }
+            finally
+            {
+                dbConn.Close();
+            }
+        }
 
+        private void btnAddTech_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            string insertQuery = "INSERT INTO Technician(name) VALUES ('" + txtTechName.Text + "')";
+            executeQuery(insertQuery);
+            displayTech();
+            txtTechName.Text = "";
+        }
+
+        private void btnEditTech_Click(object sender, EventArgs e)
+        {
+            buttonClicked = true;
+            string selectedTech = dgvTechList.CurrentRow.Cells[0].Value.ToString();
+            string editQuery = "UPDATE Technician SET name = '" + txtTechName.Text + "' WHERE technicianId = '" + selectedTech + "'";
+            executeQuery(editQuery);
+            displayTech();
+            txtTechName.Text = "";
+        }
+
+        private void btnDeleteTech_Click(object sender, EventArgs e)
+        {
+            buttonClicked = false;
+            if (dgvTechList.SelectedCells.Count > 0)
+            {
+                if (MessageBox.Show("Confirm to delete technician", "Warning", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    string selectedIndex = dgvTechList.CurrentRow.Cells[0].Value.ToString();
+                    string deleteQuery = "DELETE FROM Technician WHERE technicianId = '" + selectedIndex + "'";
+                    executeQuery(deleteQuery);
+                    displayTech();
+                    txtTechName.Text = "";
+                }
+            }
+        }
+
+        private void dgvTechList_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtTechName.Text = dgvTechList.CurrentRow.Cells[1].Value.ToString();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
